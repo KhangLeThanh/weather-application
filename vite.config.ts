7ef94dee2b843823f1a48ff7d599048df0f5eca0
Khang Le Thanh <khangthanh2312@gmail.com>
2026-05-08 12:52:00 +0700

    fixing unit test on pipelines

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
@@ -8,5 +9,11 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "./src/test/setup.ts",
     include: ["src/**/*.test.{ts,tsx}"],
+    alias: [
+      {
+        find: /^.+\.module\.scss$/,
+        replacement: path.resolve(__dirname, "./src/test/styleMock.ts"),
+      },
+    ],
   },
 });
